f5039bc2ceb348be0f356956707a126ddacfe39a
Rakesh <rakeshp22052005@gmail.com>
2024-06-24 21:25:33 +0530

    committed

--- backend/index.js
+++ backend/index.js
@@ -33,13 +33,161 @@ mongoClient.connect(process.env.DB_URL)
 //     res.json("hello")
 // })
 
-const adminApp=require('./adminApi')
-
-app.use('/admin-api',adminApp)
-
 // app.use((req,res,next)=>{
 //     res.sendFile(path.join(__dirname,'../frontend/build/index.html'))
 // })
+
+app.get('/hackathon',async(req,res)=>{
+    let resObj=await hackathonCollection.findOne()
+    if(resObj!=null){
+        res.send({
+            message:'Success',
+            data:{
+                past:resObj.past,
+                current:resObj.current,
+                upcoming:resObj.upcoming
+            }
+        })
+    }
+})
+
+app.get('/explore',async(req,res)=>{
+    let resObj=await exploreCollection.findOne()
+    if(resObj!=null){
+        res.send({
+            message:'Success',
+            data:{
+                ideas:resObj.ideas,
+                alumini:resObj.alumini
+            }
+        })
+    }
+    else{
+        res.send({
+            message:'Error occurred'
+        })
+    }
+})
+
+app.post('/hackathon/past',async(req,res)=>{
+    let obj=req.body
+    let resObj1=await hackathonCollection.findOne()
+    if(resObj1!=null){
+        resObj1.past.push(obj)
+        let resObj2=await hackathonCollection.updateOne({},{$set:{past:resObj1.past}})
+        if(resObj2.modifiedCount>0){
+            res.send({
+                message:'Success'
+            })
+        }
+        else{
+            res.send({
+                message:'Error updating'
+            })
+        }
+    }
+    else{
+        res.send({
+            message:'Error occurred'
+        })
+    }
+})
+
+app.post('/hackathon/current',async(req,res)=>{
+    let obj=req.body
+    let resObj1=await hackathonCollection.findOne()
+    if(resObj1!=null){
+        resObj1.current.push(obj)
+        let resObj2=await hackathonCollection.updateOne({},{$set:{current:resObj1.current}})
+        if(resObj2.modifiedCount>0){
+            res.send({
+                message:'Success'
+            })
+        }
+        else{
+            res.send({
+                message:'Error updating'
+            })
+        }
+    }
+    else{
+        res.send({
+            message:'Error occurred'
+        })
+    }
+})
+
+app.post('/hackathon/upcoming',async(req,res)=>{
+    let obj=req.body
+    let resObj1=await hackathonCollection.findOne()
+    if(resObj1!=null){
+        resObj1.upcoming.push(obj)
+        let resObj2=await hackathonCollection.updateOne({},{$set:{upcoming:resObj1.upcoming}})
+        if(resObj2.modifiedCount>0){
+            res.send({
+                message:'Success'
+            })
+        }
+        else{
+            res.send({
+                message:'Error updating'
+            })
+        }
+    }
+    else{
+        res.send({
+            message:'Error occurred'
+        })
+    }
+})
+
+app.post('/explore/ideas',async(req,res)=>{
+    let obj=req.body
+    let resObj1=await exploreCollection.findOne()
+    if(resObj1!=null){
+        resObj1.ideas.push(obj)
+        let resObj2=await exploreCollection.updateOne({},{$set:{ideas:resObj1.ideas}})
+        if(resObj2.modifiedCount>0){
+            res.send({
+                message:'Success'
+            })
+        }
+        else{
+            res.send({
+                message:'Error updating'
+            })
+        }
+    }
+    else{
+        res.send({
+            message:'Error occurred'
+        })
+    }
+})
+
+app.post('/explore/alumini',async(req,res)=>{
+    let obj=req.body
+    let resObj1=await exploreCollection.findOne()
+    if(resObj1!=null){
+        resObj1.alumini.push(obj)
+        let resObj2=await exploreCollection.updateOne({},{$set:{alumini:resObj1.alumini}})
+        if(resObj2.modifiedCount>0){
+            res.send({
+                message:'Success'
+            })
+        }
+        else{
+            res.send({
+                message:'Error updating'
+            })
+        }
+    }
+    else{
+        res.send({
+            message:'Error occurred'
+        })
+    }
+})
 
 app.use((err,req,res,next)=>{
     res.send({
